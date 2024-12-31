9ee6d8e32bbbb7b1a8d00d16d8363296c16158c3
Eliyor <eliyor4101@gmail.com>
2024-12-31 19:15:36 +0500

    Yangi darslar joylandi
    17,18-Darslar joylandi

---
+++ 17-dars..py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 31 13:14:49 2024
+
+@author: MS1517
+"""
+ism=input('Ismingizni kiriting: ')
+savol=f'Salom, {ism.title()}. Yoshingiz nechchida?'
+yosh=input(savol)
+yosh=int(yosh)
+height=input('Boyingiz nechchi metr? ')
+height=float(height)
+
+
+
+
+
+
+
---
+++ 18-dars.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 31 17:36:06 2024
+
+@author: MS1517
+"""
+
