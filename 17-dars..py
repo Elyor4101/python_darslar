5be823224d70529fd065051e6d4208d02b4fab3d
Eliyor <67564851+Elyor4101@users.noreply.github.com>
2025-01-19 18:18:12 +0500

    ivgaierugvo
    sipa

--- 17-dars..py
+++ 17-dars..py
@@ -4,16 +4,87 @@ Created on Tue Dec 31 13:14:49 2024
 
 @author: MS1517
 """
+"""
 ism=input('Ismingizni kiriting: ')
 savol=f'Salom, {ism.title()}. Yoshingiz nechchida?'
 yosh=input(savol)
 yosh=int(yosh)
 height=input('Boyingiz nechchi metr? ')
 height=float(height)
-a=5
-b=6
-c=a+b
-print(c)
+"""
+"""
+son=1
+while son<=5:
+    print(son,end=' ')
+    son+=1
+    
+print('Dastur tugadi')
+
+a=3 
+while a<=9 :
+    print(a,end=' ')
+    a=a+1
+"""
+"""
+print('Kiritilgan sonning kvadratini qaytruvchi dastur.')
+savol='Istalgan sonni kiriting'
+savol+="(dasturni to'xtatish uchun 'exit' deb yozing):"
+qiymat=' '
+while qiymat !='exit': 
+    qiymat=input(savol)
+    if qiymat !='exit':
+        print(float(qiymat)**2)
+print('Dastur tugadi.')
+ """ 
+""" 
+#Ishora bilan 
+print('Kiritilgan sonning kvadratini qaytruvchi dastur.')
+savol='Istalgan sonni kiriting'
+savol+="(dasturni to'xtatish uchun 'exit' deb yozing):"
+ishora=True
+while ishora:
+    qiymat=input(savol)
+    if qiymat =='exit':
+        ishora=False
+    else:
+        print(float(qiymat)**2)
+print('Dastur tugadi!')
+
+print('Kiritilgan sonning kvadratini qaytruvchi dastur.')
+savol='Istalgan sonni kiriting'
+savol+="(dasturni to'xtatish uchun 'exit' deb yozing):"
+while True:
+    qiymat=input(savol)
+    if qiymat =='exit':
+        break
+    else:
+        print(float(qiymat)**2)
+print('Dastur tugadi!')
+"""
+#break 
+"""
+sonlar=list(range(1,11))
+for son in sonlar:
+    if son ==5:
+        break
+    print(f"{son} ning kvadrati {son**2} ga teng")
+
+#continue
+
+sonlar=list(range(1,11))
+for son in sonlar:
+    if son ==5:
+        continue
+    print(f"{son} ning kvadrati {son**2} ga teng")
+
+son=0
+while son<10:
+    son+=1
+    if son%2!=0:
+        continue
+    else:
+        print(son)
+"""
 
 
 
--- 18-dars.py
+++ 18-dars.py
@@ -4,4 +4,72 @@ Created on Tue Dec 31 17:36:06 2024
 
 @author: MS1517
 """
+'''
+print("Yaqin do'slaringiz ro'yhatini tuzamiz")
+ismlar=[ ]
+n=1
+while True:
+    savol=f"{n}-do'slaingiz ismini kiriting:"
+    ism=input(savol)
+    ismlar.append(ism)
+    takrorlash=input("Yana ism kiritasizmi (ha/yo'q)")
+    n+=1
+    if takrorlash !='ha':
+        break
+
+print("Do'stlaringiz ro'yxati:")
+for ism in ismlar:
+    print(ism.title())
+    
+'''
+'''
+print("Do'slaringiz yoshini saqlaymiz")
+dostlar={}
+ishora=True
+while ishora:
+    ism=input("Do'stlaringiz ismini kiriting:")
+    yosh=input(f"{ism.title()}ning yoshini kiriting: ")
+    dostlar[ism]=int(yosh)
+    javob=input("yana ma'lumot qo'shasizmi: (ha/yo'q)")
+    if javob =="yo'q":
+        ishora=False
+for ism,yosh in dostlar.items():
+    print(f"{ism.title()} {yosh} yoshda")
+print(dostlar)
+'''
+'''
+cars=['nexia','jentra','gelik','nexia','mers','bmv','nexia']
+while 'nexia' in cars:
+    cars.remove('nexia')
+print(cars)
+'''
+talabalar=['botir','rustam','nodir','farrux']
+baholangan_talabalar={}
+while talabalar:
+    talaba=talabalar.pop()
+    baho=input(f"{talaba.title()}ning bahosini kiriting:")
+    print(f"{talaba.title()}  baholandi")
+    baholangan_talabalar[talaba]=int(baho)
+print(baholangan_talabalar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
